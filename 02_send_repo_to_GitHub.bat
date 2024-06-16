7159162fd9d0e2c8467853f3e04d0c54165a5580
pcbproj <pcbproj@yandex.ru>
2024-06-16 12:33:06 +0300

    next commit

---
+++ 02_send_repo_to_GitHub.bat
@@ -0,0 +1,7 @@
+echo "на сайте GitHub создать репозиторий новый  и сайт выдаст команды. которые вписать, чтобы залить локальный репозиторий на GitHub"
+
+git remote add origin https://github.com/pcbproj/mcu_1wire_lock.git
+git branch -M main
+git push -u origin main
+
+pause
